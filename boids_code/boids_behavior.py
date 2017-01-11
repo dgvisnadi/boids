de2cc9dfcd60de8c95062e58febc58ecb33b50b7
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-11 18:50:33 +0000

    function fix

--- boids_code/boids_behavior.py
+++ boids_code/boids_behavior.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class Boids_fly(object):
+# class Boids_fly(object):
     # def __init__(self, start, end):
     #     self.start=start
     #     self.end=end
@@ -16,36 +16,37 @@ class Boids_fly(object):
     #     return np.vstack([lats, longs]).transpose()
 
 
-    def boids_fly_middle(coordinates, velocity):
-        param = 0.01
-        for iter_i in range(len(coordinates)):
-            for iter_j in range(len(coordinates)):
-                velocity[iter_i] = velocity[iter_i] + (coordinates[iter_j] - coordinates[iter_j]) \
-                                    * param / len(coordinates)
+def boids_fly_middle(coordinates, velocity):
+    param = 0.01
+    for i in range(50):
+        for j in range(50):
+            velocity[i]=velocity[i]+(coordinates[j]-coordinates[i])*0.01/len(coordinates)
+    return velocity
 
-    def boids_fly_away(boids):
-        xs,ys,xvs,yvs=boids
-        var_multiplier = 2
-        var_boundary = 100
-        for boid_i in range(boids_number):
-            for boid_j in range(boids_number):
-                if (xs[boid_j]-xs[boid_i])**var_multiplier + (ys[boid_j]-ys[boid_i])**var_multiplier < var_boundary:
-                    xvs[boid_i]=xvs[boid_i]+(xs[boid_i]-xs[boid_j])
-                    yvs[boid_i]=yvs[boid_i]+(ys[boid_i]-ys[boid_j])
 
-    def match_speed(boids):
-        xs,ys,xvs,yvs=boids
-        var_multiplier = 2
-        var_boundary = 10000
-        param = 0.125
-        for iter_i in range(boids_number):
-            for iter_j in range(boids_number):
-                if (xs[j]-xs[i])**var_multiplier + (ys[j]-ys[i])**var_multiplier < var_boundary:
-                    xvs[i]=xvs[i]+(xvs[j]-xvs[i])*param/len(xs)
-                    yvs[i]=yvs[i]+(yvs[j]-yvs[i])*param/len(xs)
+def boids_fly_away(xs,ys,velocity):
+    var_multiplier = 2
+    var_boundary = 100
+    boids_number = 50
+    for boid_i in range(boids_number):
+        for boid_j in range(boids_number):
+            if (xs[boid_j]-xs[boid_i])**var_multiplier + (ys[boid_j]-ys[boid_i])**var_multiplier < var_boundary:
+                velocity[boid_i]=velocity[boid_i]+(xs[boid_i]-xs[boid_j])
+    return velocity
 
-    def velocity_move(boids):
-        xs,ys,xvs,yvs=boids
-        for i in range(boids_number):
-            xs[i]=xs[i]+xvs[i]
-            ys[i]=ys[i]+yvs[i]
+def match_speed(xs,ys,velocity):
+    boids_number = 50
+    var_multiplier = 2
+    var_boundary = 10000
+    param = 0.125
+    for i in range(boids_number):
+        for j in range(boids_number):
+            if (xs[j]-xs[i])**var_multiplier + (ys[j]-ys[i])**var_multiplier < var_boundary:
+                velocity[i]=velocity[i]+(velocity[j]-velocity[i])*param/len(xs)
+    return velocity
+
+def velocity_move(coordinates,velocity):
+    boids_number = 50
+    for i in range(boids_number):
+        coordinates[i]=coordinates[i]+velocity[i]
+    return coordinates
