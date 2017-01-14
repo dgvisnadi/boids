279c385d4cc68824627d04c1420b0d5338113ca6
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-14 21:36:57 +0000

    added dependencies

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name = 'Boids',
     version = '0.1.0',
-    description= 'XXX',
+    description= 'Flying boids simulation',
     packages = find_packages(exclude=['*tests']),
     scripts = ['scripts/boids'],
-    # install_requires = ['argparse', 'geopy', 'matplotlib', 'numpy', 'requests', 'seaborn']
+    install_requires = ['argparse', 'random', 'matplotlib', 'numpy', 'yaml', 'seaborn']
 )
