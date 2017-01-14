a8f36eed444abef98dc11be819bcf3e2cb93b064
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-14 21:28:59 +0000

    config file added

--- boids_code/boids.py
+++ boids_code/boids.py
@@ -3,33 +3,30 @@ from matplotlib import animation
 from boids_behavior import *
 import random
 import seaborn
+import yaml
 
 # Variables
-def boids_basics(number=50):
-    boids_number = number
-    var_x_min, var_x_max, var_y_min, var_y_max = -450, 50, 300, 600
-    var_velo_x_min, var_velo_x_max, var_velo_y_min, var_velo_y_max = 0, 10, -20, 20
+config=yaml.load(open("config.yaml"))
 
-    boids_x = [random.uniform(var_x_min, var_x_max) for boid in range(boids_number)]
-    boids_y = [random.uniform(var_y_min, var_y_max) for boid in range(boids_number)]
-    boid_x_velocities = [random.uniform(var_velo_x_min, var_velo_x_max) for boid in range(boids_number)]
-    boid_y_velocities = [random.uniform(var_velo_y_min, var_velo_y_max) for boid in range(boids_number)]
-    boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-    return boids
+boids_number = config['number']
+x_min, x_max, y_min, y_max = config['position']
+velo_x_min, velo_x_max, velo_y_min, velo_y_max = config['velocity']
 
+boids_x = random_uniform(var_x_min, var_x_max, boids_number)
+boids_y = random_uniform(var_y_min, var_y_max, boids_number)
+boid_x_velocities = random_uniform(var_velo_x_min, var_velo_x_max, boids_number)
+boid_y_velocities = random_uniform(var_velo_y_min, var_velo_y_max, boids_number)
+boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+
+
+# Run boids flying behavior
 def update_boids(boids):
 	boids_x, boids_y, boid_x_velocities, boid_y_velocities = boids
-	# Fly towards the middle
 	boid_x_velocities, boid_y_velocities = boids_fly_middle(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	# Fly away from nearby boids
 	boid_x_velocities, boid_y_velocities = boids_fly_away(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	# Try to match speed with nearby boids
 	boid_x_velocities, boid_y_velocities = match_speed(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	# Move according to velocities
 	boids_x, boids_y = velocity_move(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
-
-boids = boids_basics(50)
 
 # Prepare Graphic
 figure = plt.figure()
