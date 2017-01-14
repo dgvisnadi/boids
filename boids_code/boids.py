e14f58317edc475f238fba68b0e9849b3d63c276
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-14 23:49:57 +0000

    update function update with number of boids

--- boids_code/boids.py
+++ boids_code/boids.py
@@ -21,10 +21,10 @@ import yaml
 # Run boids flying behavior
 def update_boids(boids):
 	boids_x, boids_y, boid_x_velocities, boid_y_velocities = boids
-	boid_x_velocities, boid_y_velocities = boids_fly_middle(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	boid_x_velocities, boid_y_velocities = boids_fly_away(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	boid_x_velocities, boid_y_velocities = match_speed(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
-	boids_x, boids_y = velocity_move(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+	boid_x_velocities, boid_y_velocities = boids_fly_middle(boids_x, boids_y, boid_x_velocities, boid_y_velocities, len(boids))
+	boid_x_velocities, boid_y_velocities = boids_fly_away(boids_x, boids_y, boid_x_velocities, boid_y_velocities, len(boids))
+	boid_x_velocities, boid_y_velocities = match_speed(boids_x, boids_y, boid_x_velocities, boid_y_velocities, len(boids))
+	boids_x, boids_y = velocity_move(boids_x, boids_y, boid_x_velocities, boid_y_velocities, len(boids))
 
 
 # # Prepare Graphic
--- boids_code/boids_behavior.py
+++ boids_code/boids_behavior.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 
-boids_number = 50
+# boids_number = 50
 # class Boids_fly(object):
 #     # def __init__(self, start, end):
 #     #     self.start=start
@@ -14,7 +14,7 @@ def random_uniform(min_values, max_values, boids_number):
     vector = [random.uniform(min_values, max_values) for boid in range(boids_number)]
     return vector
 
-def boids_fly_middle(x_coordinate, y_coordinate, velocity_x, velocity_y):
+def boids_fly_middle(x_coordinate, y_coordinate, velocity_x, velocity_y, boids_number):
     param = 0.01
     for i in range(boids_number):
         for j in range(boids_number):
@@ -23,7 +23,7 @@ def boids_fly_middle(x_coordinate, y_coordinate, velocity_x, velocity_y):
     return velocity_x, velocity_y
 
 # Fly away from nearby boids
-def boids_fly_away(x_coordinate, y_coordinate, velocity_x, velocity_y):
+def boids_fly_away(x_coordinate, y_coordinate, velocity_x, velocity_y, boids_number):
     var_multiplier = 2
     var_boundary = 100
     for i in range(boids_number):
@@ -34,7 +34,7 @@ def boids_fly_away(x_coordinate, y_coordinate, velocity_x, velocity_y):
     return velocity_x, velocity_y
 
 # Try to match speed with nearby boids
-def match_speed(x_coordinate, y_coordinate, velocity_x, velocity_y):
+def match_speed(x_coordinate, y_coordinate, velocity_x, velocity_y, boids_number):
     var_multiplier = 2
     var_boundary = 10000
     param = 0.125
@@ -46,7 +46,7 @@ def match_speed(x_coordinate, y_coordinate, velocity_x, velocity_y):
     return velocity_x, velocity_y
 
 # Move according to velocities
-def velocity_move(x_coordinate, y_coordinate, velocity_x, velocity_y):
+def velocity_move(x_coordinate, y_coordinate, velocity_x, velocity_y, boids_number):
     for i in range(boids_number):
         x_coordinate[i] = x_coordinate[i] + velocity_x[i]
         y_coordinate[i] = y_coordinate[i] + velocity_y[i]
