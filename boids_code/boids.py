c551b3b1ee893397fbd66519bd8dfc28a543e22d
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-14 20:15:32 +0000

    created basic function that defines input

--- boids_code/boids.py
+++ boids_code/boids.py
@@ -5,23 +5,17 @@ import random
 import seaborn
 
 # Variables
-boids_number = 50
+def boids_basics(number=50):
+    boids_number = number
+    var_x_min, var_x_max, var_y_min, var_y_max = -450, 50, 300, 600
+    var_velo_x_min, var_velo_x_max, var_velo_y_min, var_velo_y_max = 0, 10, -20, 20
 
-var_x_min = -450
-var_x_max = 50
-var_y_min = 300
-var_y_max = 600
-
-var_velo_x_min = 0
-var_velo_x_max = 10
-var_velo_y_min = -20
-var_velo_y_max = 20
-
-boids_x = [random.uniform(var_x_min, var_x_max) for boid in range(boids_number)]
-boids_y = [random.uniform(var_y_min, var_y_max) for boid in range(boids_number)]
-boid_x_velocities = [random.uniform(var_velo_x_min, var_velo_x_max) for boid in range(boids_number)]
-boid_y_velocities = [random.uniform(var_velo_y_min, var_velo_y_max) for boid in range(boids_number)]
-boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+    boids_x = [random.uniform(var_x_min, var_x_max) for boid in range(boids_number)]
+    boids_y = [random.uniform(var_y_min, var_y_max) for boid in range(boids_number)]
+    boid_x_velocities = [random.uniform(var_velo_x_min, var_velo_x_max) for boid in range(boids_number)]
+    boid_y_velocities = [random.uniform(var_velo_y_min, var_velo_y_max) for boid in range(boids_number)]
+    boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+    return boids
 
 def update_boids(boids):
 	boids_x, boids_y, boid_x_velocities, boid_y_velocities = boids
@@ -35,6 +29,8 @@ def update_boids(boids):
 	boids_x, boids_y = velocity_move(boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
 
+boids = boids_basics(50)
+
 # Prepare Graphic
 figure = plt.figure()
 figure.suptitle('Boids simulation')
@@ -44,8 +40,8 @@ scatter = axes.scatter(boids[0], boids[1])
 
 # Animate and fill graphic
 def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(list(zip(boids[0],boids[1])))
+   	update_boids(boids)
+   	scatter.set_offsets(list(zip(boids[0],boids[1])))
 
 # Execute boids animation
 anim = animation.FuncAnimation(figure, animate,frames=50, interval=50)
