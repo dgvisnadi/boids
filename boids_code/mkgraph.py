ff3f2b0d14b435b44dd1cf4d9f2a8788aa5bf5b7
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-15 19:55:17 +0000

    added number of boids in graph title

--- boids_code/mkgraph.py
+++ boids_code/mkgraph.py
@@ -13,7 +13,7 @@ def make_graph(number, out=False):
     number: str     - How many boids appear in the animation
     end: str        -
     steps: int      -
-    out: str        - Type 'mp4' or 'gif' to save the animation
+    out: str        - Type mp4 or gif to save the animation
     '''
 
 
