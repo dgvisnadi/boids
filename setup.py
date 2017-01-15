4a54223d3eed85d8fd5757e2b2a069699936c836
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-15 15:52:59 +0000

    added yaml file setup parameter

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     description= 'Flying boids simulation',
     packages = find_packages(exclude=['*tests']),
     scripts = ['scripts/boids'],
+    data_files=[('config', ['boids_code/config.yaml'])],
     install_requires = ['argparse', 'matplotlib', 'numpy', 'pyyaml', 'seaborn']
 )
