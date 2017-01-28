1390d4844860cfe9c7dde53eabf18fa670ed99e2
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-01-28 14:02:49 +0000

    added speed parameter to command line

--- boids_code/mkgraph.py
+++ boids_code/mkgraph.py
@@ -5,7 +5,7 @@ import seaborn
 import yaml
 import os
 
-def make_graph(number, speed=0, out=False):
+def make_graph(number, out=False):
     '''Generates an animated plot simulating boids flying.
 
     Parameters
@@ -19,14 +19,14 @@ def make_graph(number, speed=0, out=False):
     # Variables
     config=yaml.load(open(os.path.join(os.path.dirname(__file__),'config.yaml')))
 
-    if speed == 0:
-        speed = config['velocity']['slow']
-    elif speed == 1:
-        speed = config['velocity']['fast']
+    # if speed == 0:
+    #     speed = config['velocity']['slow']
+    # elif speed == 1:
+    #     speed = config['velocity']['fast']
 
     boids_number = number
     x_min, x_max, y_min, y_max = config['position']
-    velo_x_min, velo_x_max, velo_y_min, velo_y_max = speed
+    velo_x_min, velo_x_max, velo_y_min, velo_y_max = config['velocity']['slow']
 
     boids_x = random_uniform(x_min, x_max, boids_number)
     boids_y = random_uniform(y_min, y_max, boids_number)
