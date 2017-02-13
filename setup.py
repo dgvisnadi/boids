332fab9c3edd62b79c6d62641b8152eb4e448919
Dimitri Visnadi <dgvisnadi@gmail.com>
2017-02-13 14:15:23 +0100

    added author details

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'Boids',
     version = '0.1.0',
     description= 'Flying boids simulation',
-    author= 'Dimitri Visnadi'
+    author= 'Dimitri Visnadi',
     packages = find_packages(exclude=['*tests']),
     scripts = ['scripts/boids'],
     package_data = {"boids_code": ["config.yaml"]},
